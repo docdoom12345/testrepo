81f3006e9a8ef9813226f7eaf3ab350bc16372bf
docdoom12345 <docdoom12345@outlook.com>
2022-04-15 13:49:09 +0530

    new update

--- first.tf
+++ first.tf
@@ -1,6 +1,10 @@
-#my first file
-provider "azurerm"{
-    features {
-      
-    }
+provider "azurerm" {
+  features {}
+}
+resource "azurerm_storage_account" "example" {
+  name                     = "mystorageacc10"
+  resource_group_name      = "myrg"
+  location                 = "eastus"
+  account_tier             = "Standard"
+  account_replication_type = "GRS"
 }
