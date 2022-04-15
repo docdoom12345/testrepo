4c3beb399de682ca8b0269859c8abc0a59d284cb
docdoom12345 <docdoom12345@outlook.com>
2022-04-15 15:33:14 +0530

    first.tf second update v1

--- first.tf
+++ first.tf
@@ -7,8 +7,8 @@ resource "azurerm_resource_group" "example"{
 }
 resource "azurerm_storage_account" "example" {
   name                     = "mystorageacc10"
-  resource_group_name      = "myrg"
-  location                 = "eastus"
+  resource_group_name      = azurerm_resource_group.example.name
+  location                 = azurerm_resource_group.example.location
   account_tier             = "Standard"
   account_replication_type = "GRS"
 }
