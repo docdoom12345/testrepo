8ac3f38c67854c36f54a3476ad14e9b3db878dd8
docdoom12345 <docdoom12345@outlook.com>
2022-04-19 19:57:40 +0530

    uploading new files

--- first.tf
+++ first.tf
@@ -1,14 +1,84 @@
 provider "azurerm" {
   features {}
+  subscription_id = "b2d3029d-a6ac-4690-b005-312bba3a7639" #subscription ID
+  client_id       = "129b5913-a8e3-4fd5-8f41-f3e2a2547f63" #appID
+  client_secret   = "ej3MhK1xUFK9C-YCo370w1HeTCuibFQECV" #password
+  tenant_id       = "cea297cb-9bde-428d-9a6e-48fa9c582ed6" #tenant
 }
-resource "azurerm_resource_group" "example"{
-    name = var.rg[0]
-    location = var.rg[1]
+resource "azurerm_resource_group" "example" {
+  name     = "example-resources"
+  location = "West US"
 }
-resource "azurerm_storage_account" "example" {
-  name                     = "mystorageacc10"
-  resource_group_name      = azurerm_resource_group.example.name
-  location                 = azurerm_resource_group.example.location
-  account_tier             = "Standard"
-  account_replication_type = "GRS"
+
+resource "azurerm_virtual_network" "example" {
+  name                = "example-network"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+}
+
+resource "azurerm_subnet" "example" {
+  name                 = "internal"
+  resource_group_name  = azurerm_resource_group.example.name
+  virtual_network_name = azurerm_virtual_network.example.name
+  address_prefixes     = ["10.0.2.0/24"]
+}
+resource "azurerm_public_ip" "example" {
+  name                = "acceptanceTestPublicIp1"
+  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.example.location
+  allocation_method   = "Static"
+}
+resource "azurerm_network_interface" "example" {
+  name                = "example-nic"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.example.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.example.id
+  }
+}
+
+resource "azurerm_linux_virtual_machine" "example" {
+  name                            = "example-machine"
+  resource_group_name             = azurerm_resource_group.example.name
+  location                        = azurerm_resource_group.example.location
+  size                            = "Standard_DS1_v2"
+  admin_username                  = "adminuser"
+  admin_password                  = "ubuntu@123"
+  disable_password_authentication = false
+  network_interface_ids = [
+    azurerm_network_interface.example.id,
+  ]
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "18.04-LTS"
+    version   = "latest"
+  }
+  provisioner "remote-exec" {
+    inline = [
+      "sudo apt-get update -y",
+      "sudo apt-get install nginx -y",
+      "sudo systemctl start nginx"
+    ]
+  }
+    connection {
+      type     = "ssh"
+      user     = self.admin_username
+      password = self.admin_password
+      host     = self.public_ip_address    
+  }
+}
+output "mypublicip"{
+  value = azurerm_public_ip.example.ip_address
 }
