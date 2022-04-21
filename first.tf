b4f164f9d11957652a2634b866261c670905e654
docdoom12345 <103731163+docdoom12345@users.noreply.github.com>
2022-04-21 16:35:58 +0530

    Update first.tf

--- first.tf
+++ first.tf
@@ -1,9 +1,5 @@
 provider "azurerm" {
   features {}
-  subscription_id = "b2d3029d-a6ac-4690-b005-312bba3a7639" #subscription ID
-  client_id       = "129b5913-a8e3-4fd5-8f41-f3e2a2547f63" #appID
-  client_secret   = "ej3MhK1xUFK9C-YCo370w1HeTCuibFQECV" #password
-  tenant_id       = "cea297cb-9bde-428d-9a6e-48fa9c582ed6" #tenant
 }
 resource "azurerm_resource_group" "example"{
     name = var.rg[0]
